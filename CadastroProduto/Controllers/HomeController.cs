95745ed7e3c598c84800eae14389b5b3b298ccb1
Israel Curvelo <raellcurvelo@gmail.com>
2020-06-26 14:31:41 -0300

    Cadastro

---
+++ CadastroProduto/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CadastroProduto.Models;
+using CadastroProduto.Models.Domain;
+
+namespace CadastroProduto.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexUsuario(Usuario usuario)
+        {
+            return View(usuario);
+        }
+
+        public IActionResult Create()
+        {
+            return RedirectToAction("Create","Usuarios");
+        }
+
+        public IActionResult CreateU()
+        {
+            return RedirectToAction("Create", "Produtos");
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CadastroProduto/Controllers/ProdutosController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CadastroProduto.Dal;
+using CadastroProduto.Data;
+using CadastroProduto.Facade;
+using CadastroProduto.Models.Domain;
+using CadastroProduto.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroProduto.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly DataBaseContext dbContext;
+        
+        public ProdutosController(DataBaseContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index(Produto produtos)
+        {
+            
+            Produto produto = new Produto();
+            ProdutoFacade uf = new ProdutoFacade(dbContext);
+
+            List<Produto> resultado = new List<Produto>();
+            foreach (EntidadeDominio x in uf.Consultar(produto))
+            {
+                resultado.Add((Produto)x);
+            }
+            return View(resultado);
+            
+           // return View();
+        }
+
+        public IActionResult Create()
+        {
+            /*
+            EntidadeDominio fichaTecnica = new FichaTecnica();
+            FichaTecnicaFacade lf = new FichaTecnicaFacade(dbContext);
+
+            List<FichaTecnica> resultadoFichaTecnica = new List<FichaTecnica>();
+            foreach (EntidadeDominio x in lf.Consultar(fichaTecnica))
+            {
+                resultadoFichaTecnica.Add((FichaTecnica)x);
+            }
+            EntidadeDominio cliente = new Cliente();
+            ClienteFacade cf = new ClienteFacade(dbContext);
+
+            List<Cliente> resultadoCliente = new List<Cliente>();
+            foreach (EntidadeDominio x in cf.Consultar(cliente))
+            {
+                resultadoCliente.Add((Cliente)x);
+            }
+
+
+            var viewModel = new ProdutoViewModel { FichaTecnica = resultadoFichaTecnica, Cliente = resultadoCliente };
+            return View(viewModel);
+            */
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Produto produto)
+        {
+            
+            ProdutoFacade cf = new ProdutoFacade(dbContext);
+            cf.Cadastrar(produto);
+            
+
+            return RedirectToAction("Create","Acessorios",produto.Linha);
+        }
+    }
+}
